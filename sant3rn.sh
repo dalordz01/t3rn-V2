2fa4d93b972c0b8948169a02549917dd470e77a9
Saandy <38919673+KaelVNode@users.noreply.github.com>
2025-03-20 21:13:14 +0700

    Update sant3rn.sh

--- sant3rn.sh
+++ sant3rn.sh
@@ -22,6 +22,16 @@ EXECUTOR_USER=${EXECUTOR_USER:-root}
 read -sp "Masukkan PRIVATE_KEY_LOCAL: " PRIVATE_KEY_LOCAL
 echo ""
 
+# Prompt Alchemy API
+echo -n "API Key Alchemy: "
+read APIKEY_ALCHEMY
+echo
+
+# Prompt Gas Price
+echo -n "Gas Price: "
+read GAS_PRICE
+echo
+
 INSTALL_DIR="/home/$EXECUTOR_USER/t3rn"
 SERVICE_FILE="/etc/systemd/system/t3rn-executor.service"
 ENV_FILE="/etc/t3rn-executor.env"
@@ -33,13 +43,15 @@ mkdir -p "$INSTALL_DIR" && cd "$INSTALL_DIR"
 TAG=$(curl -s https://api.github.com/repos/t3rn/executor-release/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
 wget "https://github.com/t3rn/executor-release/releases/download/$TAG/executor-linux-$TAG.tar.gz"
 
+
 # Ekstrak file
 tar -xzf executor-linux-*.tar.gz
 cd executor/executor/bin
 
+
 # Konfigurasi environment file
 sudo bash -c "cat > $ENV_FILE" <<EOL
-RPC_ENDPOINTS="{\"l2rn\": [\"https://b2n.rpc.caldera.xyz/http\"], \"arbt\": [\"https://arbitrum-sepolia.drpc.org\", \"https://sepolia-rollup.arbitrum.io/rpc\"], \"bast\": [\"https://base-sepolia-rpc.publicnode.com\", \"https://base-sepolia.drpc.org\"], \"opst\": [\"https://sepolia.optimism.io\", \"https://optimism-sepolia.drpc.org\"], \"unit\": [\"https://unichain-sepolia.drpc.org\", \"https://sepolia.unichain.org\"]}"
+RPC_ENDPOINTS="{\"l2rn\": [\"https://b2n.rpc.caldera.xyz/http\"], \"arbt\": [\"https://arbitrum-sepolia.drpc.org\", \"https://arb-sepolia.g.alchemy.com/v2/$APIKEY_ALCHEMY\"], \"bast\": [\"https://base-sepolia-rpc.publicnode.com\", \"https://base-sepolia.g.alchemy.com/v2/$APIKEY_ALCHEMY\"], \"opst\": [\"https://sepolia.optimism.io\", \"https://opt-sepolia.g.alchemy.com/v2/$APIKEY_ALCHEMY\"], \"unit\": [\"https://unichain-sepolia.g.alchemy.com/v2/$APIKEY_ALCHEMY\", \"https://sepolia.unichain.org\"]}"
 EOL
 
 # Berikan hak akses ke user
@@ -64,7 +76,9 @@ Environment=LOG_PRETTY=false
 Environment=EXECUTOR_PROCESS_BIDS_ENABLED=true
 Environment=EXECUTOR_PROCESS_ORDERS_ENABLED=true
 Environment=EXECUTOR_PROCESS_CLAIMS_ENABLED=true
-Environment=EXECUTOR_MAX_L3_GAS_PRICE=100
+Environment=EXECUTOR_PROCESS_PENDING_ORDERS_FROM_API=false
+Environment=EXECUTOR_PROCESS_ORDERS_API_ENABLED=false
+Environment=EXECUTOR_MAX_L3_GAS_PRICE=$GAS_PRICE
 Environment=PRIVATE_KEY_LOCAL=$PRIVATE_KEY_LOCAL
 Environment=ENABLED_NETWORKS=arbitrum-sepolia,base-sepolia,optimism-sepolia,l2rn
 EnvironmentFile=$ENV_FILE
@@ -80,5 +94,5 @@ sudo systemctl enable t3rn-executor.service
 sudo systemctl start t3rn-executor.service
 
 # Tampilkan log secara real-time
-echo "✅ Executor berhasil diinstall dan dijalankan! Menampilkan log real-time..."
+echo "✅ Executor berhasil diinstall dan siap dikewer-kewer! Menampilkan log real-time.."
 sudo journalctl -u t3rn-executor.service -f --no-hostname -o cat
