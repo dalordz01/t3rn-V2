b599b27be0cc4adf223200611ed7eb1754e95075
Saandy <38919673+KaelVNode@users.noreply.github.com>
2025-03-22 09:50:29 +0700

    Update sant3rnv2.sh

--- sant3rnv2.sh
+++ sant3rnv2.sh
@@ -78,7 +78,6 @@ RPC_ENDPOINTS='{
   "l2rn": ["https://b2n.rpc.caldera.xyz/http"],
   "arbt": ["https://arbitrum-sepolia.drpc.org", "https://arb-sepolia.g.alchemy.com/v2/$APIKEY_ALCHEMY"],
   "bast": ["https://base-sepolia-rpc.publicnode.com", "https://base-sepolia.g.alchemy.com/v2/$APIKEY_ALCHEMY"],
-  "blst": ["https://sepolia.blast.io", "https://blast-sepolia.g.alchemy.com/v2/$APIKEY_ALCHEMY"],
   "opst": ["https://sepolia.optimism.io", "https://opt-sepolia.g.alchemy.com/v2/$APIKEY_ALCHEMY"],
   "unit": ["https://unichain-sepolia.drpc.org", "https://unichain-sepolia.g.alchemy.com/v2/$APIKEY_ALCHEMY"]
 }'
